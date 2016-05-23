515a184c56382fca9dcfef42630004bf3114bece
Richard <79544198@qq.com>
2016-05-23 21:53:42 +0800

    DB modify By Richard 20160523

--- 03_Database/SQL/create_t_course_info.sql
+++ 03_Database/SQL/create_t_course_info.sql
@@ -1,11 +1,11 @@
-DROP TABLE IF EXISTS `t_course_info`;
+﻿DROP TABLE IF EXISTS `t_course_info`;
 CREATE TABLE `t_course_info` (
   `no` bigint(12) NOT NULL AUTO_INCREMENT,
   `courseNo` varchar(16) NOT NULL,
   `releaseDate` timestamp NOT NULL,
   `releaseFlg` char(1) NOT NULL,
   `courseName` varchar(100) DEFAULT NULL,
-  `courseDetails` text(10000),
+  `courseDetails` blob,
   `addTimestamp` timestamp NULL DEFAULT NULL,
   `addUserKey` varchar(40) DEFAULT NULL,
   `updTimestamp` timestamp DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
--- 03_Database/SQL/create_t_sys_resource.sql
+++ 03_Database/SQL/create_t_sys_resource.sql
@@ -1,4 +1,4 @@
-DROP TABLE IF EXISTS `t_sys_resource`;
+﻿DROP TABLE IF EXISTS `t_sys_resource`;
 CREATE TABLE `t_sys_resource` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `resourceName` varchar(100) NOT NULL,
@@ -8,4 +8,5 @@ CREATE TABLE `t_sys_resource` (
   `parentResourceIds` varchar(200) DEFAULT NULL,
   `resourcePermission` varchar(100) DEFAULT NULL,
   `resourceAvailable` char(1),
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
