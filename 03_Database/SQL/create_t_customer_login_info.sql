95691f8b444cf0154868496fb2d330ca9ff562ea
Richard1112 <79544198@qq.com>
2016-12-21 09:00:29 +0800

    no message

--- 03_Database/SQL/create_t_customer_login_info.sql
+++ 03_Database/SQL/create_t_customer_login_info.sql
@@ -5,7 +5,7 @@ CREATE TABLE `t_customer_login_info` (
   `loginId` varchar(50) NOT NULL,
   `loginPass` varchar(255) NOT NULL,
   `salt` varchar(48) DEFAULT NULL,
-  `division` char(1) NOT NULL,
+  `division` char(1) DEFAULT NULL,
   `loginStatus` char(1),
   `canLogin` char(1),
   `deleteFlg` char(1) NOT NULL,
